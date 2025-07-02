f5f141c60386fa822e506056db5fce9752b6d74a
unknown <stevielyfiles@gmail.com>
2025-07-02 10:12:19 +0700

    commit 2 pages more

--- app/aboutUs/page.jsx
+++ app/aboutUs/page.jsx
@@ -1,32 +1,21 @@
+import Image from "next/image";
+
 export default function Page() {
   return (
-    <div>
-      <h1>AboutUs</h1>
-      <h1>AboutUs</h1>
-      <h1>AboutUs</h1>
-      <h1>AboutUs</h1>
-      <h1>AboutUs</h1>
-      <h1>AboutUs</h1>
-      <h1>AboutUs</h1>
-      <h1>AboutUs</h1>
-      <h1>AboutUs</h1>
-      <h1>AboutUs</h1>
-      <h1>AboutUs</h1>
-      <h1>AboutUs</h1>
-      <h1>AboutUs</h1>
-      <h1>AboutUs</h1>
-      <h1>AboutUs</h1>
-      <h1>AboutUs</h1>
-      <h1>AboutUs</h1>
-      <h1>AboutUs</h1>
-      <h1>AboutUs</h1>
-      <h1>AboutUs</h1>
-      <h1>AboutUs</h1>
-      <h1>AboutUs</h1>
-      <h1>AboutUs</h1>
-      <h1>AboutUs</h1>
-      <h1>AboutUs</h1>
-      <h1>AboutUs</h1>
+    <div className="flex flex-col w-full overflow-x-hidden">
+      <div className="relative w-full -mb-px">
+        <Image src="/ab1.svg" alt="background1" width={1920} height={1080} className="w-full h-auto object-contain" />
+      </div>
+      <div className="relative w-full -mb-px">
+        <Image src="/ab2.svg" alt="background1" width={1920} height={1080} className="w-full h-auto object-contain" />
+      </div>
+      <div className="relative w-full -mb-px">
+        <Image src="/ab3.svg" alt="background1" width={1920} height={1080} className="w-full h-auto object-contain" />
+      </div>
+      <div className="relative w-full -mb-px">
+        <Image src="/ab4.svg" alt="background1" width={1920} height={1080} className="w-full h-auto object-contain" />
+      </div>
+     
     </div>
   );
 }
